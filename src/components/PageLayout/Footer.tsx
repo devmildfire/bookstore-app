95ba766f01d3e863b0f7428fe856e7ad4de44404
Fer-NaN-des <aleksandr.sleptsov@mail.ru>
2021-09-30 22:58:12 +0300

    start footer component

--- src/components/PageLayout/Footer.tsx
+++ src/components/PageLayout/Footer.tsx
@@ -1,9 +1,59 @@
 import React from 'react';
+import Link from 'next/link';
 
 import styles from './PageLayout.module.css';
 
 const Footer = (): React.ReactElement => (
-  <div className={styles.footer}>Footer</div>
+  <div className={styles.footer}>
+    <div className={styles.container}>
+      <div className={styles.footerTitle}>
+        <span>Чти</span>
+        <span>во</span>
+      </div>
+      <div className={styles.links}>
+        <Link href='/to-readers' passHref>
+          <a href='fakeHref'>Чтецам</a>
+        </Link>
+        <Link href='/to-authors' passHref>
+          <a href='fakeHref'>Авторам</a>
+        </Link>
+        <Link href='/to-partners' passHref>
+          <a href='fakeHref'>Партнерам</a>
+        </Link>
+        <Link href='/about' passHref>
+          <a href='fakeHref'>О Чтиве</a>
+        </Link>
+        <Link href='/contacts' passHref>
+          <a href='fakeHref'>Контакты</a>
+        </Link>
+      </div>
+      <div className={styles.contacts}>
+        <a
+          href='https://russiandino.ru/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          Создано Русским Динозавром
+        </a>
+        <a href='tel:+78129158367'>
+          Тел. (812) 915-83-67
+        </a>
+        <a
+          href='mailto:info@chtivo.spb.ru'
+          target='_top'
+        >
+          E-mail info@chtivo.spb.ru
+        </a>
+      </div>
+      <div className={styles.copyright}>
+        <span>
+          © 2017-2021&nbsp;
+          <span>Чти</span>
+          во. Санкт-Петербург. Все права защищены.
+        </span>
+      </div>
+    </div>
+  </div>
 );
 
 export default Footer;
