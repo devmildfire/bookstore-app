51957c0bd9d65eede9ef216d50441664579b6e49
Alexander Lyz <a.lyz@dunice.net>
2021-12-20 01:05:19 +0300

    add abstract popper component

--- src/components/PageLayout/components/HeaderTab.tsx
+++ src/components/PageLayout/components/HeaderTab.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useRef, useMemo } from 'react';
-import { usePopper } from 'react-popper';
+import React from 'react';
 import Link from 'next/link';
 import styled from 'styled-components';
+
+import Popper from '../../Popper';
 
 export type MenuItem = {
   title: string,
@@ -32,76 +33,32 @@ const HeaderTab = ({
   title,
   link,
   submenu,
-}: IHeaderTab): React.ReactElement => {
-  const [showPopper, setShowPopper] = useState(false);
+}: IHeaderTab): React.ReactElement => (
+  <>
+    {!submenu
+      ? (
+        <Link href={link} passHref>
+          <StyledLink href='fakeHref'>{title}</StyledLink>
+        </Link>
+      )
+      : (
+        <Popper
+          target={
+            <StyledLink>{title}</StyledLink>
+          }
+          padding={20}
+        >
+          <PopperContainer>
+            {submenu?.map((tab) => (
+              <Link href={tab.link} passHref key={tab.title}>
+                <StyledLink href='fakeHref'>{tab.title}</StyledLink>
+              </Link>
+            ))}
+          </PopperContainer>
+        </Popper>
 
-  const buttonRef = useRef(null);
-  const popperRef = useRef(null);
-
-  const [arrowRef, setArrowRef] = useState<HTMLDivElement | null>(null);
-
-  const offset = useMemo(
-    () => ({
-      name: 'offset',
-      options: {
-        offset: ({ reference, popper }: any) => [(popper.width - reference.width) / 2 - 20, 0],
-      },
-    }),
-    [],
-  );
-
-  const { styles, attributes } = usePopper(
-    buttonRef.current,
-    popperRef.current,
-    {
-      modifiers: [
-        {
-          name: 'arrow',
-          options: {
-            element: arrowRef,
-          },
-        },
-        offset,
-      ],
-    },
-  );
-  return (
-    <>
-      {!submenu
-        ? (
-          <Link href={link} passHref>
-            <StyledLink href='fakeHref'>{title}</StyledLink>
-          </Link>
-        )
-        : (
-          <>
-            <StyledLink
-              ref={buttonRef}
-              onMouseEnter={() => setShowPopper(true)}
-              onMouseLeave={() => setShowPopper(false)}
-            >
-              {title}
-            </StyledLink>
-            {showPopper ? (
-              <PopperContainer
-                ref={popperRef}
-                onMouseEnter={() => setShowPopper(true)}
-                onMouseLeave={() => setShowPopper(false)}
-                style={styles.popper}
-                {...attributes.popper}
-              >
-                <div ref={setArrowRef} style={styles.arrow} id='arrow' />
-                {submenu?.map((tab) => (
-                  <Link href={tab.link} passHref key={tab.title}>
-                    <StyledLink href='fakeHref'>{tab.title}</StyledLink>
-                  </Link>
-                ))}
-              </PopperContainer>
-            ) : null}
-          </>
-        )}
-    </>
-  );
-};
+      )}
+  </>
+);
 
 export default HeaderTab;
