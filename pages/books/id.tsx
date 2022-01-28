77e004721812c4e04d83a8e50fa4b2227a9dd1d7
Alexmiko <mr.coctail@mail.ru>
2022-01-28 21:57:38 +0300

    fix bags for linter

--- pages/books/id.tsx
+++ pages/books/id.tsx
@@ -5,198 +5,217 @@ import { ReactSVG } from 'react-svg';
 import colors from '../../src/utils/colors';
 import Button from '../../src/components/Common/Button';
 
-const BookPage = (): React.ReactElement =>
-// const {
-//     title,
-//     link,
-//     author,
-//     yearOfPublication,
-//     genre,
-//     ageRestriction,
-//     description,
-// } = book;
-// const parsedDescription = description
-//     .map((paragraph) => <p className='descriptionParagraph'>{paragraph}</p>);
-
-  (
-    <StyleWrapper>
-      <Head>
-        <title>DELETED</title>
-      </Head>
-      <Book>
-        <BookImage
-          src='/images/bookTitleDeleted_BookPage.jpg'
-          alt='Book logo'
-        />
-        <BookInfo>
-          <BookTitle>
-            DELETED
-          </BookTitle>
-          <BookAuthor>
-            Катерина Кюне
-          </BookAuthor>
-          <BookProps>
-            2021 | роман |18+
-          </BookProps>
-          <BookThesis>
-            ЕСЛИ ВЫ НЕ УСПЕЛИ ПОПРОЩАТЬСЯ С БАБУЛЕЙ,
-            МЫ ПЕРЕДАДИМ ВАШЕ СООБЩЕНИЕ
-          </BookThesis>
-          <BookDescrText>
-            <p className='bookDescrParagraph'>
-              Стася работает бардонавткой,
-              кем-то вроде почтальона между нашим миром и
-              Бардо — так учёные назвали случайно открытое измерение,
-              куда на некоторое время после смерти попадает сознание умерших людей.
-            </p>
-            <p className='bookDescrParagraph'>
-              Стася хорошо себя чувствует среди мёртвых, а вот в мире живых у неё полно проблем: письма и слежка бывшего парня, постоянные разговоры отца о её никчёмности…
-            </p>
-            <p className='bookDescrParagraph'>
-              Но всего этого как будто недостаточно, и в её жизни появляется ещё один преследователь — невидимый.
-            </p>
-          </BookDescrText>
-        </BookInfo>
-      </Book>
-      <Props>
-        <PropsHeader>
-          <PropsTitle>
-            ЦИФРОВОЕ ИЗДАНИЕ
-          </PropsTitle>
-          <PropsPrice>
-            300₽
-          </PropsPrice>
-          <PropsDate>
-            Дата релиза: дд.мм.гггг
-          </PropsDate>
-        </PropsHeader>
-        <PropsBody>
-          <PropsBtnBlock>
-            <Button
-              text='Добавить в корзину'
-              className='propsBtn'
-            />
-            <Button
-              text='Демо-версия'
-              className='propsBtn'
-            />
-          </PropsBtnBlock>
-          <PropsItems>
-            <PropsItem>
-              Форматы: Fb2, Epub
-            </PropsItem>
-            <PropsItem>
-              Кол-во символов: 355000
-            </PropsItem>
-            <PropsItem>
-              <span>
-                Рекомендуемые читалки:
-              </span>
-              <ReadersList>
-                <ReadersItem >
-                  eBoox: Android | iPhone
-                </ReadersItem>
-                <ReadersItem>
-                  FBReader: Android | iPhone
-                </ReadersItem>
-                <ReadersItem>
-                  KyBooks: iPhone
-                </ReadersItem>
-              </ReadersList>
-            </PropsItem>
-          </PropsItems>
-        </PropsBody>
-        <PropsFooter>
-          Над изданием работали: редактор Наталья Кислова, веб-мастер Серафим Лоза, дизайнер Екатерина Яковлева, верстальщик Леон Меликьянц, иллюстратор Евгений Борщевский
-        </PropsFooter>
-      </Props>
-      <Trailer>
-        <Title>
-          Буктрейлер
-        </Title>
-        <TrailerVideo
-          src='/images/trailerScreenShot.png'
-          alt='Trailer'
-        />
-      </Trailer>
-      <Author>
-        <Title>
-          Об авторе
-        </Title>
-        <AuthorInfo>
-          <AuthorFoto
-            src='/images/authors/kune.jpg'
-            alt='Автор'
+const BookPage = (): React.ReactElement => (
+  <StyleWrapper>
+    <Head>
+      <title>DELETED</title>
+    </Head>
+    <Book>
+      <BookImage
+        src='/images/bookTitleDeleted_BookPage.jpg'
+        alt='Book logo'
+      />
+      <BookInfo>
+        <BookTitle>
+          DELETED
+        </BookTitle>
+        <BookAuthor>
+          Катерина Кюне
+        </BookAuthor>
+        <BookProps>
+          2021 | роман |18+
+        </BookProps>
+        <BookThesis>
+          ЕСЛИ ВЫ НЕ УСПЕЛИ ПОПРОЩАТЬСЯ С БАБУЛЕЙ,
+          МЫ ПЕРЕДАДИМ ВАШЕ СООБЩЕНИЕ
+        </BookThesis>
+        <BookDescrText>
+          <p className='bookDescrParagraph'>
+            Стася работает бардонавткой,
+            кем-то вроде почтальона между нашим миром и
+            Бардо — так учёные назвали случайно открытое измерение,
+            куда на некоторое время после смерти попадает сознание умерших людей.
+          </p>
+          <p className='bookDescrParagraph'>
+            Стася хорошо себя чувствует среди мёртвых,
+            а вот в мире живых у неё полно проблем: письма и слежка бывшего парня,
+            постоянные разговоры отца о её никчёмности…
+          </p>
+          <p className='bookDescrParagraph'>
+            Но всего этого как будто недостаточно,
+            и в её жизни появляется ещё один преследователь — невидимый.
+          </p>
+        </BookDescrText>
+      </BookInfo>
+    </Book>
+    <Props>
+      <PropsHeader>
+        <PropsTitle>
+          ЦИФРОВОЕ ИЗДАНИЕ
+        </PropsTitle>
+        <PropsPrice>
+          300₽
+        </PropsPrice>
+        <PropsDate>
+          Дата релиза: дд.мм.гггг
+        </PropsDate>
+      </PropsHeader>
+      <PropsBody>
+        <PropsBtnBlock>
+          <Button
+            text='Добавить в корзину'
+            className='propsBtn'
           />
-          <AuthorDescr>
-            <AuthorProps>
-              Катерина Кюне | Аскер | 24.03.1984
-            </AuthorProps>
-            <AuthorSpeech>
-              «Мне всегда нравилось представлять себя кем-то другим: собакой, тюльпаном, соседом дядей Васей, путешественницей к другим планетам. В детстве я так играла. Мне хотелось прожить много разных жизней, попробовать много разных занятий. Писательство — это реинкарнация без необходимости умирать. Можно оказаться там, куда тебе не добраться физически, исследовать то, к чему у тебя нет доступа. И даже то, чего не существует. Я просто продолжаю играть, вот и всё»
-            </AuthorSpeech>
-          </AuthorDescr>
-        </AuthorInfo>
-        <AuthorAbout>
-          Катерина Кюне, родилась в 1984 году в Магадане. По семейной легенде, родители нашли её в громадном сугробе, который намело под их окнами. В детстве писала стихи и песни, придумывала страшные истории, которыми пугала подруг. Училась в Санкт-Петербургском университете телекоммуникаций, но бросила после второго курса. Переехала в Москву. Окончила Литературный институт имени Горького. Работала методистом, корреспондентом, копирайтером, координатором благотворительного фонда, выпускающим редактором, репетитором по математике, продавцом-буфетчицей, разработчиком электронных курсов. Делала лампы из мусора. Занималась научной журналистикой. На заказ написала историческую биографию предков одного из российских олигархов. Книга была дорого издана и богата иллюстрирована, но кроме семьи олигарха её никто не прочёл. Жила в разных городах: в Магадане, Майкопе, Санкт-Петербурге, Москве, Севастополе, Ярославле, Бангкоке, Берлине. Сейчас живёт в Аскере — норвежском городке рядом с Осло. Зарабатывает трейдингом. Имеет публикации в журналах «Знамя», «Эмигрантская лира», «Дружба народов», «Лиterraтура», «Этажи», «Берлин. Берега» (Германия) и других. Лауреат премии литературного журнала «Знамя». Автор повести «Здесь должна быть я».
-        </AuthorAbout>
-        <AuthorContacts>
-          <span>
-            Контакты:
-          </span>
-          <ContactsList>
-            <ContactsItem>
-              <ContactsLink
-                href='mailto:example@example.com'
-                target='_blank'
-              >
-                <ReactSVG src='/email.svg' />
-              </ContactsLink>
-            </ContactsItem>
-            <ContactsItem>
-              <ContactsLink
-                href='http://instagram.com'
-                target='_blank'
-              >
-                <ReactSVG src='/instagram.svg' />
-              </ContactsLink>
-            </ContactsItem>
-            <ContactsItem>
-              <ContactsLink
-                href='http://facebook.com'
-                target='_blank'
-              >
-                <ReactSVG src='/facebook.svg' />
-              </ContactsLink>
-            </ContactsItem>
-            <ContactsItem>
-              <ContactsLink
-                href='http://t.me/username'
-                target='_blank'
-              >
-                <ReactSVG src='/telegram.svg' />
-              </ContactsLink>
-            </ContactsItem>
-            <ContactsItem>
-              <ContactsLink
-                href='http://vk.com'
-                target='_blank'
-              >
-                <ReactSVG src='/vk.svg' />
-              </ContactsLink>
-            </ContactsItem>
-          </ContactsList>
-        </AuthorContacts>
-      </Author>
-      <Similar>
-        <SimilarTitle>
-          Познайте также
-        </SimilarTitle>
-        <SimilarBooks>
-        </SimilarBooks>
-      </Similar>
-    </StyleWrapper>
-  );
+          <Button
+            text='Демо-версия'
+            className='propsBtn'
+          />
+        </PropsBtnBlock>
+        <PropsItems>
+          <PropsItem>
+            Форматы: Fb2, Epub
+          </PropsItem>
+          <PropsItem>
+            Кол-во символов: 355000
+          </PropsItem>
+          <PropsItem>
+            <span>
+              Рекомендуемые читалки:
+            </span>
+            <ReadersList>
+              <ReadersItem>
+                eBoox: Android | iPhone
+              </ReadersItem>
+              <ReadersItem>
+                FBReader: Android | iPhone
+              </ReadersItem>
+              <ReadersItem>
+                KyBooks: iPhone
+              </ReadersItem>
+            </ReadersList>
+          </PropsItem>
+        </PropsItems>
+      </PropsBody>
+      <PropsFooter>
+        Над изданием работали: редактор Наталья Кислова,
+        веб-мастер Серафим Лоза, дизайнер Екатерина Яковлева,
+        верстальщик Леон Меликьянц, иллюстратор Евгений Борщевский
+      </PropsFooter>
+    </Props>
+    <Trailer>
+      <Title>
+        Буктрейлер
+      </Title>
+      <TrailerVideo
+        src='/images/trailerScreenShot.png'
+        alt='Trailer'
+      />
+    </Trailer>
+    <Author>
+      <Title>
+        Об авторе
+      </Title>
+      <AuthorInfo>
+        <AuthorFoto
+          src='/images/authors/kune.jpg'
+          alt='Автор'
+        />
+        <AuthorDescr>
+          <AuthorProps>
+            Катерина Кюне | Аскер | 24.03.1984
+          </AuthorProps>
+          <AuthorSpeech>
+            «Мне всегда нравилось представлять себя кем-то другим:
+            собакой, тюльпаном, соседом дядей Васей, путешественницей к другим планетам.
+            В детстве я так играла. Мне хотелось прожить много разных жизней,
+            попробовать много разных занятий. Писательство —
+            это реинкарнация без необходимости умирать.
+            Можно оказаться там, куда тебе не добраться физически,
+            исследовать то, к чему у тебя нет доступа.
+            И даже то, чего не существует. Я просто продолжаю играть, вот и всё»
+          </AuthorSpeech>
+        </AuthorDescr>
+      </AuthorInfo>
+      <AuthorAbout>
+        Катерина Кюне, родилась в 1984 году в Магадане.
+        По семейной легенде, родители нашли её в громадном сугробе,
+        который намело под их окнами. В детстве писала стихи и песни,
+        придумывала страшные истории, которыми пугала подруг.
+        Училась в Санкт-Петербургском университете телекоммуникаций,
+        но бросила после второго курса. Переехала в Москву.
+        Окончила Литературный институт имени Горького.
+        Работала методистом, корреспондентом, копирайтером, координатором благотворительного фонда,
+        выпускающим редактором, репетитором по математике, продавцом-буфетчицей,
+        разработчиком электронных курсов.
+        Делала лампы из мусора. Занималась научной журналистикой.
+        На заказ написала историческую биографию предков одного из российских олигархов.
+        Книга была дорого издана и богата иллюстрирована,
+        но кроме семьи олигарха её никто не прочёл.
+        Жила в разных городах: в Магадане, Майкопе, Санкт-Петербурге,
+        Москве, Севастополе, Ярославле, Бангкоке, Берлине.
+        Сейчас живёт в Аскере — норвежском городке рядом с Осло.
+        Зарабатывает трейдингом. Имеет публикации в журналах «Знамя»,
+        «Эмигрантская лира», «Дружба народов»,
+        «Лиterraтура», «Этажи», «Берлин. Берега» (Германия) и других.
+        Лауреат премии литературного журнала «Знамя». Автор повести «Здесь должна быть я».
+      </AuthorAbout>
+      <AuthorContacts>
+        <span>
+          Контакты:
+        </span>
+        <ContactsList>
+          <ContactsItem>
+            <ContactsLink
+              href='mailto:example@example.com'
+              target='_blank'
+            >
+              <ReactSVG src='/email.svg' />
+            </ContactsLink>
+          </ContactsItem>
+          <ContactsItem>
+            <ContactsLink
+              href='http://instagram.com'
+              target='_blank'
+            >
+              <ReactSVG src='/instagram.svg' />
+            </ContactsLink>
+          </ContactsItem>
+          <ContactsItem>
+            <ContactsLink
+              href='http://facebook.com'
+              target='_blank'
+            >
+              <ReactSVG src='/facebook.svg' />
+            </ContactsLink>
+          </ContactsItem>
+          <ContactsItem>
+            <ContactsLink
+              href='http://t.me/username'
+              target='_blank'
+            >
+              <ReactSVG src='/telegram.svg' />
+            </ContactsLink>
+          </ContactsItem>
+          <ContactsItem>
+            <ContactsLink
+              href='http://vk.com'
+              target='_blank'
+            >
+              <ReactSVG src='/vk.svg' />
+            </ContactsLink>
+          </ContactsItem>
+        </ContactsList>
+      </AuthorContacts>
+    </Author>
+    <Similar>
+      <SimilarTitle>
+        Познайте также
+      </SimilarTitle>
+      <SimilarBooks>
+      </SimilarBooks>
+    </Similar>
+  </StyleWrapper>
+);
 
 export default BookPage;
 
@@ -430,4 +449,6 @@ const SimilarTitle = styled.h2`
   line-height: 53px;
   color: ${colors.red};
 `;
-const SimilarBooks = styled.div``;
+const SimilarBooks = styled.div`
+
+`;
