876e6db2023b42b04b43623b29c9d4e6dc7596af
mildfire <mildfire@gmail.com>
2023-01-22 22:05:30 +0500

    ReadCafe удалён из списка партнёров

--- src/mocks/partners.ts
+++ src/mocks/partners.ts
@@ -1,5 +1,4 @@
 import { Partner } from '@/types/partner';
-import readCafePhoto from '../../public/images/partners/read-cafe.webp';
 import logo451 from '../../public/images/partners/451.png';
 import poryadokSlov from '../../public/images/partners/poryadok-slov.png';
 import ahuli from '../../public/images/partners/ahuli.png';
@@ -17,76 +16,71 @@ import factotum from '../../public/images/partners/factotum.png';
 const partners: Partner[] = [
   {
     id: 0,
-    name: 'ReadCafe',
-    photo: readCafePhoto.src,
-  },
-  {
-    id: 1,
     name: '451',
     photo: logo451.src,
     displayName: 'фаренгейт 451',
   },
   {
-    id: 2,
+    id: 1,
     name: 'Порядок Слов',
     photo: poryadokSlov.src,
   },
   {
-    id: 3,
+    id: 2,
     name: 'Ахули',
     photo: ahuli.src,
     displayName: 'Ахули',
   },
   {
-    id: 4,
+    id: 3,
     name: 'Дискурс',
     photo: discourse.src,
     displayName: 'Дискурс',
   },
   {
-    id: 5,
+    id: 4,
     name: 'Смена',
     photo: smena.src,
   },
   {
-    id: 6,
+    id: 5,
     name: 'Подписные издания',
     photo: subscriptions.src,
   },
   {
-    id: 7,
+    id: 6,
     name: 'Ночлежка',
     photo: flophouse.src,
   },
   {
-    id: 8,
+    id: 7,
     name: 'Фаланстер',
     photo: falanster.src,
   },
   {
-    id: 9,
+    id: 8,
     name: 'Колми',
     photo: callme.src,
     displayName: 'Колми',
   },
   {
-    id: 10,
+    id: 9,
     name: 'Год литературы',
     photo: literatureyear.src,
     displayName: 'Год литературы',
   },
   {
-    id: 11,
+    id: 10,
     name: 'Дорога жизни',
     photo: liferoad.src,
   },
   {
-    id: 12,
+    id: 11,
     name: 'Книгиподарки',
     photo: bookspresents.src,
   },
   {
-    id: 13,
+    id: 12,
     name: 'Фактотум',
     photo: factotum.src,
     displayName: 'Фактотум',
