522063fcd4395edd79cd87f093bc4f1aa5494748
sergromm <sergichev@gmail.com>
2023-02-28 19:41:54 +0300

    экспорты для splitByRows, useScreenSize и useScrollTo

---
+++ src/utils/splitByRows.ts
@@ -0,0 +1,21 @@
+/**
+ *
+ * @param array массив который нужно разбить на двумерный
+ * @param columnLength длина вложенных массивов
+ * @returns двумерный массив
+ */
+const splitByRows = <T extends unknown>(array: T[], columnLength: number) => {
+  let row = 0;
+  const reducer = (grid: T[][], curr: T) => {
+    if (grid[row].length === columnLength) {
+      row += 1;
+      grid.push([]);
+    }
+    grid[row].push(curr);
+    return grid;
+  };
+
+  return array.reduce(reducer, [[]]);
+};
+
+export default splitByRows;
