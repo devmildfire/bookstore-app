74c03a47c2329f6dc4ddc508624aeeea03e557d8
mildfire <mildfire@gmail.com>
2023-03-08 00:34:55 +0500

    Заменён ref в компоненте Marquee

--- src/components/Common/Marquee/Marquee.tsx
+++ src/components/Common/Marquee/Marquee.tsx
@@ -41,10 +41,10 @@ export default function Marquee(
   props: PropsWithChildren<MarqueeProps>
 ): React.ReactElement {
   const { speed, gap, direction, delay = 0, children } = props;
-  const { ref, time, width } = useMarqueeTiming<HTMLUListElement>(speed);
+  const { ref, time, width } = useMarqueeTiming<HTMLDivElement>(speed);
   return (
-    <MarqueeWrapper gap={gap}>
-      <MarqueeContent ref={ref} time={time} direction={direction} delay={delay}>
+    <MarqueeWrapper ref={ref} gap={gap}>
+      <MarqueeContent time={time} direction={direction} delay={delay}>
         {children}
       </MarqueeContent>
       {width}
