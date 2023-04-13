8018389a09729acccfb1a16a1bbef2420e59f87f
mildfire <mildfire@gmail.com>
2023-04-13 22:39:02 +0500

    добавлена страница Инвесторам, пока пустая

---
+++ pages/for-investors/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next';
+import React from 'react';
+import styled from 'styled-components';
+// import breakPoints from '@/utils/breakPoints';
+
+const ForInvestors: NextPage = () => {
+  return <StyledWrapper></StyledWrapper>;
+};
+
+const StyledWrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default ForInvestors;
