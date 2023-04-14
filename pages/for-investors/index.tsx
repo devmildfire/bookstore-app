503662b964971222172fdc5f2422d4e97ed4f946
devmildfire F <mildfire@gmail.com>
2023-04-14 11:42:37 +0000

    Сетка с макетами графика

--- pages/for-investors/index.tsx
+++ pages/for-investors/index.tsx
@@ -1,16 +1,39 @@
 import type { NextPage } from 'next';
 import React from 'react';
 import styled from 'styled-components';
+import stonks from '@/assets/images/Vector_Stonks.svg';
 // import breakPoints from '@/utils/breakPoints';
 
 const ForInvestors: NextPage = () => {
-  return <StyledWrapper></StyledWrapper>;
+  return <StyledWrapper>
+    <Stonks1 as={stonks as any}/>
+    <Stonks2 as={stonks as any}/>
+  </StyledWrapper>;
 };
 
 const StyledWrapper = styled.main`
-  display: flex;
-  flex-direction: column;
+  // display: flex;
+  // flex-direction: column;
   align-items: center;
+
+  display: grid;
+  grid-template-areas: 
+    "title title" 
+    "content stonks2"
+    "stonks1 .";
 `;
+
+const Stonks1 = styled.svg`
+  grid-area: stonks1; 
+  height: 20vh; 
+  width: 50vw; 
+`;
+
+const Stonks2 = styled.svg`
+  grid-area: stonks2; 
+  height: 20vh; 
+  width: 50vw; 
+`;
+
 
 export default ForInvestors;
