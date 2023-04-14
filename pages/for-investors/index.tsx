464e4bf0e230c25ccb5d56a65c201ba95280fee1
devmildfire F <mildfire@gmail.com>
2023-04-14 11:55:49 +0000

    Сетка с макетами графика и заголовка

--- pages/for-investors/index.tsx
+++ pages/for-investors/index.tsx
@@ -2,18 +2,30 @@ import type { NextPage } from 'next';
 import React from 'react';
 import styled from 'styled-components';
 import stonks from '@/assets/images/Vector_Stonks.svg';
+import Text from '@/components/Common/Text';
 // import breakPoints from '@/utils/breakPoints';
+
+const title = 'Инвесторам и донаторам';
 
 const ForInvestors: NextPage = () => {
   return <StyledWrapper>
+    <ToInvestors variant='h3_1Man' className='title'>
+        {title}
+    </ToInvestors>
     <Stonks1 as={stonks as any}/>
     <Stonks2 as={stonks as any}/>
   </StyledWrapper>;
 };
 
+const ToInvestors = styled(Text)`
+  grid-area: title; 
+`;
+
 const StyledWrapper = styled.main`
   // display: flex;
   // flex-direction: column;
+  * {outline: 1px solid green}
+
   align-items: center;
 
   display: grid;
