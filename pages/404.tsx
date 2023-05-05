b0ad92d2b08d8f37004357d73f640a13813f14b5
mildfire <mildfire@gmail.com>
2023-05-05 20:49:13 +0500

    добавлена кнопка возврата на главную

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,7 @@
 import Hall9000 from '@/assets/images/HAL9000.svg';
+import Button from '@/components/Common/Button';
 import { Text } from '@/components/Common/Text/Text';
+import breakPoints from '@/utils/breakPoints';
 import styled from 'styled-components';
 
 const HallIcon = styled.svg`
@@ -9,14 +11,30 @@ const HallIcon = styled.svg`
   width: auto;
 `;
 
+const HallDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5vh;
+  /* justify-items: space-around; */
+  /* height: 100%; */
+
+  padding: 10vw;
+
+  @media ${breakPoints.lg} {
+    padding: 5vw;
+  }
+`;
+
 const Hall = () => {
   return (
-    <>
+    <HallDiv>
       <HallIcon as={Hall9000} />
       <Text variant='h3_1Man' align='center'>
         I&apos;m sorry Dave, I&apos;m afraid I can&apos;t do that
       </Text>
-    </>
+      <Button href='/'> Вернуться на главную </Button>
+    </HallDiv>
   );
 };
 
