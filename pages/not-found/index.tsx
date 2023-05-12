10329bbb634c1cd49d31bafe7113561a7a337c61
mildfire <mildfire@gmail.com>
2023-05-12 23:05:18 +0500

    Новый заголовок

--- pages/not-found/index.tsx
+++ pages/not-found/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import Text from '@/components/Common/Text';
+import breakPoints from '@/utils/breakPoints';
 
 const PageContainer = styled.div`
   display: flex;
@@ -9,12 +11,48 @@ const PageContainer = styled.div`
   align-items: center;
 `;
 
-const Title = styled.h1``;
+// const Title = styled.h1``;
+
+const Title = styled(Text)`
+  padding: 0 10vw;
+  font-size: 80px;
+
+  @media screen and (max-width: 1600px) {
+    font-size: 50px;
+  }
+
+  @media ${breakPoints.xl} {
+    font-size: 50px;
+  }
+
+  @media screen and (max-width: 1200px) {
+    font-size: 40px;
+  }
+
+  @media ${breakPoints.lg} {
+    font-size: 40px;
+    padding: 0 5vw;
+  }
+
+  @media ${breakPoints.md} {
+    font-size: 20px;
+  }
+
+  @media ${breakPoints.smd} {
+    font-size: 20px;
+  }
+
+  @media ${breakPoints.sm} {
+    font-size: 20px;
+  }
+`;
 
 function NotFoundPage() {
   return (
     <PageContainer>
-      <Title>Любопытство это хорошо, но придётся ещё немного подождать</Title>
+      <Title variant='h1' align='center'>
+        Любопытство — это хорошо, но придётся ещё немного подождать
+      </Title>
     </PageContainer>
   );
 }
