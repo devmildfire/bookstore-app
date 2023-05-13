9f78a6ef455a99301fcd71b3b65c4ce5d93fd4d3
mildfire <mildfire@gmail.com>
2023-05-13 13:26:35 +0500

    Размер шрифта заголовка, центрирование кнопки, размер анимации как на старом сайте

--- pages/not-found/index.tsx
+++ pages/not-found/index.tsx
@@ -21,35 +21,35 @@ const PageContainer = styled.div`
 
 const Title = styled(Text)`
   padding: 0 10vw;
-  font-size: 80px;
+  /* font-size: 80px; */
 
   @media screen and (max-width: 1600px) {
-    font-size: 50px;
+    /* font-size: 50px; */
   }
 
   @media ${breakPoints.xl} {
-    font-size: 50px;
+    /* font-size: 50px; */
   }
 
   @media screen and (max-width: 1200px) {
-    font-size: 40px;
+    /* font-size: 40px; */
   }
 
   @media ${breakPoints.lg} {
-    font-size: 40px;
+    /* font-size: 40px; */
     padding: 0 5vw;
   }
 
   @media ${breakPoints.md} {
-    font-size: 20px;
+    /* font-size: 20px; */
   }
 
   @media ${breakPoints.smd} {
-    font-size: 20px;
+    /* font-size: 20px; */
   }
 
   @media ${breakPoints.sm} {
-    font-size: 20px;
+    /* font-size: 20px; */
   }
 `;
 
