9cf704dda70b778cf9c9b72f2abaece50c3b458a
mildfire <mildfire@gmail.com>
2023-06-11 21:50:31 +0500

    Новая библиотека Embla для слайдера команды и партнёров. Теперь слайдер можно перетаскивать

--- src/components/AboutPage/We/Members.tsx
+++ src/components/AboutPage/We/Members.tsx
@@ -1,18 +1,58 @@
 import React from 'react';
-import Marquee from '@/components/Common/Marquee';
+// import Marquee from '@/components/Common/Marquee';
 import MemberCard from './MemberCard';
 import members from '@/mocks/members';
 import setUUIDField from '@/utils/setUUIDField';
 
+import useEmblaCarousel, { EmblaOptionsType } from 'embla-carousel-react';
+import styled from 'styled-components';
+import Autoplay from 'embla-carousel-autoplay';
+
 const MembersWID = setUUIDField(members);
+
+type EmblaPropType = {
+  options?: EmblaOptionsType;
+  className: string;
+};
+
+const emblaOptions = { loop: true, speed: 0.0025 };
+const autoplayOptions = { delay: 0, jump: false, stopOnInteraction: false };
+
+const EmblaCarousel: React.FC<EmblaPropType> = ({ options, className }) => {
+  const [emblaRef] = useEmblaCarousel(options, [Autoplay(autoplayOptions)]);
+
+  return (
+    <div className={className} ref={emblaRef}>
+      <div>
+        {MembersWID.map((member) => (
+          <MemberCard {...member} key={member.key} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const StyledCarousel = styled(EmblaCarousel)`
+  overflow: hidden;
+
+  > div {
+    backface-visibility: hidden;
+    display: flex;
+    touch-action: pan-y;
+    flex-direction: row;
+  }
+`;
 
 const Members = (): React.ReactElement => {
   return (
-    <Marquee speed={25} gap={0} direction='normal' delay={0}>
-      {MembersWID.map((member) => (
-        <MemberCard {...member} key={member.key} />
-      ))}
-    </Marquee>
+    <>
+      {/* <Marquee speed={25} gap={0} direction='normal' delay={0}>
+        {MembersWID.map((member) => (
+          <MemberCard {...member} key={member.key} />
+        ))}
+      </Marquee> */}
+      <StyledCarousel className='emblaWrapper' options={emblaOptions} />
+    </>
   );
 };
 
