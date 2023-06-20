570275fdc2cfe08757ffa3541e522d5bc5a4a7b3
devmildfire F <mildfire@gmail.com>
2023-06-20 14:00:25 +0000

    Новые размеры элементов

--- pages/404.tsx
+++ pages/404.tsx
@@ -17,7 +17,7 @@ const HallDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 2vh;
+  // gap: 2vh;
   /* justify-items: space-around; */
   /* height: 100%; */
 
@@ -29,12 +29,46 @@ const HallDiv = styled.div`
 `;
 
 const HalLogoStyled = styled(HalLogo)`
+  width: 100%;
   max-width: 404px;
+  padding-bottom: 90px;
+
+  @media ${breakPoints.lg} {
+    padding-bottom: 70px;
+  }
+  
+  @media ${breakPoints.md} {
+    padding-bottom: 20px;
+    max-width: 204px;
+  }
 `;
 
 const HallIconStyled = styled(HallIcon)`
   width: 100%;
-  max-width: 485px;
+  max-width: 550px;
+
+  @media ${breakPoints.md} {
+    max-width: 250px;
+  }
+`;
+
+const StyledButton = styled(Button)`
+padding-top: 50px;
+
+@media ${breakPoints.md} {
+  padding-top: 20px;
+
+  > button {
+    max-width: 217px;
+    min-width: 217px;
+
+    > p {
+      font-size: 10px;
+    }
+  }
+
+}
+
 `;
 
 const Hall = () => {
@@ -44,13 +78,17 @@ const Hall = () => {
       <HallIconStyled />
       <HalLogoStyled />
       <Text variant='h2_1' align='center'>
-        «Прости, Дэйв, боюсь, я не могу этого сделать»
+        «Прости, Дэйв, боюсь,
         {/* I&apos;m sorry Dave, I&apos;m afraid I can&apos;t do that */}
       </Text>
-      <Button href='/' variant='wide'>
+      <Text variant='h2_1' align='center'>
+        я не могу этого сделать»
+        {/* I&apos;m sorry Dave, I&apos;m afraid I can&apos;t do that */}
+      </Text>
+      <StyledButton className='backButton' href='/' variant='wide'>
         {' '}
         Вернуться на главную{' '}
-      </Button>
+      </StyledButton>
     </HallDiv>
   );
 };
