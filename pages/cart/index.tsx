ad45056ade0c7c6ab181520fda048654a9115a45
mildfire <mildfire@gmail.com>
2024-02-07 22:52:17 +0500

    new product paradigm

--- pages/cart/index.tsx
+++ pages/cart/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useCallback, useEffect, useReducer, useState } from 'react';
 import styled from 'styled-components';
 import * as Styled from '../../src/components/CartPage/CartPage.styled';
 import CartItem from '../../src/components/CartPage/CartItem/CartItem';
@@ -8,48 +8,49 @@ import ColumnLabels from '../../src/components/CartPage/ColumnLabels/ColumnLabel
 
 import { setOrGetCartCookie } from '@/utils/cardID';
 import { Cart as CartType, CartItem as CartItemType } from '@/types/api';
+import { postData } from '@/utils/postData';
 
 interface Product {
-  id: number;
+  titleId: number;
+  category: string;
   bookCover: string;
   title: string;
   author: string;
-  edition: string;
   price: number;
   oldPrice?: number;
   quantity: number;
 }
 
-const cartProductsMock: Product[] = [
-  {
-    id: 1,
-    bookCover: '/images/bookTitleDeleted.jpg',
-    author: 'Катерина Кюне',
-    edition: 'печатное',
-    title: 'DELETED',
-    price: 300,
-    oldPrice: 350,
-    quantity: 2,
-  },
-  {
-    id: 2,
-    bookCover: '/images/bookTitleDeleted.jpg',
-    author: 'Катерина Кюне',
-    edition: 'цифровое',
-    title: 'DELETED',
-    price: 300,
-    quantity: 1,
-  },
-  {
-    id: 3,
-    bookCover: '/images/bookTitleDeleted.jpg',
-    author: 'Катерина Кюне',
-    edition: 'Книга2.0',
-    title: 'DELETED',
-    price: 300,
-    quantity: 1,
-  },
-];
+// const cartProductsMock: Product[] = [
+//   {
+//     id: 1,
+//     bookCover: '/images/bookTitleDeleted.jpg',
+//     author: 'Катерина Кюне',
+//     edition: 'печатное',
+//     title: 'DELETED',
+//     price: 300,
+//     oldPrice: 350,
+//     quantity: 2,
+//   },
+//   {
+//     id: 2,
+//     bookCover: '/images/bookTitleDeleted.jpg',
+//     author: 'Катерина Кюне',
+//     edition: 'цифровое',
+//     title: 'DELETED',
+//     price: 300,
+//     quantity: 1,
+//   },
+//   {
+//     id: 3,
+//     bookCover: '/images/bookTitleDeleted.jpg',
+//     author: 'Катерина Кюне',
+//     edition: 'Книга2.0',
+//     title: 'DELETED',
+//     price: 300,
+//     quantity: 1,
+//   },
+// ];
 
 const BackIcon = styled(backLinkArrow)`
   margin-right: 5px;
@@ -78,53 +79,151 @@ enum productsActionKind {
 
 type productsAction = {
   type: productsActionKind;
-  productId: number;
+  titleId: number;
+  category: string;
 };
 
 function productsReducer(state: Product[], action: productsAction): Product[] {
   switch (action.type) {
     case productsActionKind.increment:
       return state.map((product) => {
-        if (product.id === action.productId) {
+        if (
+          product.titleId === action.titleId &&
+          product.category === action.category
+        ) {
           return { ...product, quantity: product.quantity + 1 };
         }
         return product;
       });
     case productsActionKind.decriment:
       return state.map((product) => {
-        if (product.id === action.productId && product.quantity > 1) {
+        if (
+          product.titleId === action.titleId &&
+          product.category === action.category &&
+          product.quantity > 1
+        ) {
           return { ...product, quantity: product.quantity - 1 };
         }
         return product;
       });
     case productsActionKind.remove:
-      return state.filter((product) => product.id !== action.productId);
+      return state.filter(
+        (product) =>
+          product.titleId !== action.titleId ||
+          product.category !== action.category
+      );
     default:
       return state;
   }
 }
 
 const Cart = (): React.ReactElement => {
-  const [products, dispatch] = useReducer(productsReducer, cartProductsMock);
-  const [totalPrice, setTotalPrice] = useState(calculateTotalPrice(products));
+  // const [products, dispatch] = useReducer(productsReducer, cartProductsMock);
+  // const [totalPrice, setTotalPrice] = useState(calculateTotalPrice(products));
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const [cart, setCart] = useState<CartType>([]);
+  const [cartID, setCartID] = useState('');
 
   useEffect(() => {
-    setTotalPrice(calculateTotalPrice(products));
-  }, [products]);
+    const newCartID = setOrGetCartCookie()?.toString();
 
-  function handleIncrementQuantity(productId: number) {
-    dispatch({ type: productsActionKind.increment, productId });
+    if (newCartID) {
+      setCartID(newCartID);
+    }
+  }, []);
+
+  useEffect(() => {
+    cartID && getCartFromDB(cartID);
+  }, [cartID]);
+
+  const getCartFromDB = useCallback(
+    async (id: string) => {
+      const cartItems: CartType = await postData(`/api/cart`, {
+        oper: 'fetch',
+        id: cartID,
+      });
+      console.log(
+        'fetched cart items list ... ',
+        JSON.stringify(cartItems, null, 2)
+      );
+      setCart([...cartItems]);
+    },
+    [cartID]
+  );
+
+  function findItemIndex(name: string, category: string): number {
+    const index = cart.findIndex(
+      (item) => item?.name == name && item?.category == category
+    );
+    return index;
   }
 
-  function handleDecrimentQuantity(productId: number) {
-    dispatch({ type: productsActionKind.decriment, productId });
+  function updateCart(updatedItem: CartItemType) {
+    const index = findItemIndex(updatedItem.name, updatedItem.category);
+
+    let list = [...cart];
+
+    index == -1 &&
+      (setCart([...cart.filter((item) => item.quantity !== 0), updatedItem]),
+      addItemToDB(updatedItem));
+
+    index !== -1 &&
+      ((list[index] = updatedItem),
+      (list = list.filter((item) => item.quantity !== 0)),
+      setCart([...list]),
+      updatedItem.quantity == 0
+        ? removeItemFromDB(updatedItem)
+        : updateItemInDB(updatedItem));
   }
 
-  function handleDelete(productId: number) {
-    dispatch({ type: productsActionKind.remove, productId });
+  async function updateItemInDB(item: CartItemType) {
+    const updatedItem: CartItemType = await postData(`/api/cart`, {
+      oper: 'update',
+      item: item,
+    });
+    console.log('updated item ... ', JSON.stringify(updatedItem, null, 2));
+    cartID && getCartFromDB(cartID);
   }
 
-  const productQuantity = products.reduce(
+  async function addItemToDB(item: CartItemType) {
+    const addedItem: CartItemType = await postData(`/api/cart`, {
+      oper: 'add',
+      item: item,
+    });
+    console.log('added item to list ... ', JSON.stringify(addedItem, null, 2));
+    cartID && getCartFromDB(cartID);
+  }
+
+  async function removeItemFromDB(item: CartItemType) {
+    const removedItem: CartItemType = await postData(`/api/cart`, {
+      oper: 'remove',
+      item: item,
+    });
+    console.log(
+      'removed item from list ... ',
+      JSON.stringify(removedItem, null, 2)
+    );
+    cartID && getCartFromDB(cartID);
+  }
+
+  // useEffect(() => {
+  //   setTotalPrice(calculateTotalPrice(products));
+  // }, [products]);
+
+  // function handleIncrementQuantity(productId: number) {
+  //   dispatch({ type: productsActionKind.increment, productId });
+  // }
+
+  // function handleDecrimentQuantity(productId: number) {
+  //   dispatch({ type: productsActionKind.decriment, productId });
+  // }
+
+  // function handleDelete(productId: number) {
+  //   dispatch({ type: productsActionKind.remove, productId });
+  // }
+
+  const productQuantity = cart.reduce(
     (acc, product) => acc + product.quantity,
     0
   ) as number;
@@ -134,7 +233,7 @@ const Cart = (): React.ReactElement => {
       <Styled.Title>Корзина</Styled.Title>
       <ColumnLabels />
       <Styled.ProductsList>
-        {products.map((product) => (
+        {cart.map((product) => (
           <CartItem
             key={product.id}
             {...product}
