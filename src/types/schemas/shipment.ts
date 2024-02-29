f15c7e1db4a2c3390dfa0e0c2e92b902699f1f52
mildfire <mildfire@gmail.com>
2024-02-29 13:34:51 +0500

    conditional shipment form validation

--- src/types/schemas/shipment.ts
+++ src/types/schemas/shipment.ts
@@ -1,58 +1,40 @@
 import { cartStore } from '@/store/CartStore';
 import { z } from 'zod';
 
-// phone: z
-// .string()
-// .regex(
-//   new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$'),
-//   'enter a valid phone'
-// ).optional(),
-// adress: z.string().min(2, 'city must be at least 2 characters long').optional(),
-
-
-// phone: z.string().optional(),
-// adress: z.string().optional(),
-
-// phone: z
-// .string()
-// .regex(
-//   new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$'),
-//   'enter a valid phone'
-// ).optional().or(z.literal('')),
-// adress: z.string().min(2, 'city must be at least 2 characters long').optional().or(z.literal('')),
-
-
 export const ShipmentSchema = z.object({
-  name: z.string().min(6, 'name must be at least 6 characters long').optional(),
-phone: z
-.string()
-.regex(
-  new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$'),
-  'enter a valid phone'
-).optional(),
-adress: z.string().min(2, 'city must be at least 2 characters long').optional(),
+  name: z.string().optional(),
+  phone: z.string().optional(),
+  adress: z.string().optional(),
   email: z.string().email('email musrt be a valid email'),
 })
 .refine(
   (data) => {
-console.log('cart has physical items .. ', cartStore.hasPhysicalGoods)
-console.log('name from form input .. ', data.name)
-console.log('name and physical goods ...', cartStore.hasPhysicalGoods && !!data.name)
-
-console.log('test parse phone ...', z
-.string()
-.regex(
-  new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$'),
-  'enter a valid phone'
-).optional().safeParse(''))
-
-
-  // return cartStore.hasPhysicalGoods && !!data.name
-  return true
-
+    return z
+    .string()
+    .regex(
+      new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$')
+    ).optional().safeParse(data.phone).success
   }, {
-    message: 'name field is required for physical goods',
+    message: 'phone field is required for physical goods',
+    path: ['phone']
+  }
+).refine(
+  (data) => {
+    if (cartStore.hasPhysicalGoods) {
+      return z.string().min(6).safeParse(data.name).success
+    } else return true
+  }, {
+    message: 'name must be at least 6 characters long refined',
     path: ['name']
+  }
+).refine(
+  (data) => {
+    if (cartStore.hasPhysicalGoods) {
+      return z.string().min(6).safeParse(data.adress).success
+    } else return true
+  }, {
+    message: 'adress must be at least 6 characters long',
+    path: ['adress']
   }
 )
 ;
