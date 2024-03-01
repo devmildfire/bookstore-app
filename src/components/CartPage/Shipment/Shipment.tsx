4d0c13215360d429c1fd36322a6affd414a42e9a
mildfire <mildfire@gmail.com>
2024-03-01 14:11:21 +0500

    safari window open

--- src/components/CartPage/Shipment/Shipment.tsx
+++ src/components/CartPage/Shipment/Shipment.tsx
@@ -136,6 +136,8 @@ interface shipmentProps {
   totalPrice: number;
 }
 
+let sWindow: Window | null;
+
 export function Shipment({
   setStage,
   cartID,
@@ -204,7 +206,10 @@ export function Shipment({
 
     emptyCartFromDB(cartID);
 
-    window.open(payUrl, '_blank'); //  изначально в сафари этот способ открыть новое окно блокируется, возможно есть другой лучший способ перенаправить пользователя в сервис оплаты
+    // window.open(payUrl, '_blank'); //  изначально в сафари этот способ открыть новое окно блокируется, возможно есть другой лучший способ перенаправить пользователя в сервис оплаты
+    
+    sWindow && (sWindow.location = payUrl)
+
   };
 
   return (
@@ -262,7 +267,9 @@ export function Shipment({
           </FormDiv> */}
 
           {/* <ButtonDiv> */}
-          <StyledButton type='submit'>Перейти к оплате</StyledButton>
+          <StyledButton type='submit' onClick={() => {
+            sWindow = window.open()
+          }}>Перейти к оплате</StyledButton>
           {/* </ButtonDiv> */}
         </StyledForm>
       </div>
