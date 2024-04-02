40a2896baee99a7d45677f49df9edc8fbcefeb8f
mildfire <mildfire@gmail.com>
2024-04-02 17:00:15 +0500

    Editions select title

--- pages/dashboard/editions/index.tsx
+++ pages/dashboard/editions/index.tsx
@@ -5,7 +5,52 @@ import { supabase } from "api/supabase-client";
 import DashMain from "@/components/DashBoardPage/DashMain";
 import DashNav from "@/components/DashBoardPage/DashNav";
 import { LogOut } from "@/components/LoginPage/Logout";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { TitleType } from "../titles";
 
+
+const TitleEditions = () => {
+
+  const [titles, setTitles] = useState<TitleType[]>();
+
+  async function getTitles() {
+    const { data, error } = await supabase.from('Titles').select('*');
+
+    data && console.log('Titles data ... ', data);
+    error && alert(error);
+
+    data && setTitles(data);
+  }
+
+  useEffect(() => {
+    getTitles();
+  }, []);
+
+  if (!titles) {
+    return <div>zero titles found in database</div>;
+  }
+
+  return(
+    <div className='w-full'>
+      <h3> Editions </h3>
+      <div>
+      <Select>
+      <SelectTrigger className="w-[280px]">
+        <SelectValue placeholder="Select a Title" />
+      </SelectTrigger>
+      <SelectContent>
+
+        {titles.map((title) => (
+          <SelectItem value={`item-${title.id}`} key={title.id}> {title.name} </SelectItem>
+        ))}
+
+      </SelectContent>
+    </Select>  
+      </div>
+    
+    </div>
+  )
+}
 
 function Editions(): React.ReactElement {
     const [session, setSession] = useState<Session>();
@@ -33,7 +78,7 @@ function Editions(): React.ReactElement {
       <DashMain>
         <div className='text-center dark flex flex-col justify-center items-center align-middle w-full self-center space-y-16'>
           <DashNav />
-          {/* <Titleslist /> */}
+          <TitleEditions />
           {session && <LogOut session={session} />}
         </div>
       </DashMain>
