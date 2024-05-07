604c3aad9a565044f6e22b68e53403554a372afe
mildfire <mildfire@gmail.com>
2024-05-07 22:58:10 +0500

    app metadata check

--- pages/dashboard/orders/index.tsx
+++ pages/dashboard/orders/index.tsx
@@ -30,7 +30,8 @@ function Orders(): React.ReactElement {
         console.error(error);
       } else {
         data.session && setSession(data.session);
-        !data.session?.user.user_metadata.isAdmin && router.push('/login');
+        // !data.session?.user.user_metadata.isAdmin && router.push('/login');
+        !data.session?.user.app_metadata.claims_admin && router.push('/login');
       }
     } catch (error) {
       console.error(error);
