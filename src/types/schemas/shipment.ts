2093616b31c67fd9af7188db0ac9446216ca7439
mildfire <mildfire@gmail.com>
2024-05-08 11:53:12 +0500

    Black background while loading animation is running

--- src/types/schemas/shipment.ts
+++ src/types/schemas/shipment.ts
@@ -7,33 +7,38 @@ import { z } from 'zod';
 export const ShipmentSchema = z
   .object({
     name: z.string().optional(),
-    phone: z.string().optional(),
+    phone: z
+      .string()
+      .min(
+        6,
+        'phone field with at least 6 digits is required for physical goods'
+      ),
     adress: z.string().optional(),
     email: z.string().email('email musrt be a valid email'),
   })
-  .refine(
-    (data) => {
-      return z
-        .string()
-        .regex(
-          new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$')
-        )
-        .optional()
-        .safeParse(data.phone).success;
-    },
-    {
-      message: 'phone field is required for physical goods',
-      path: ['phone'],
-    }
-  )
+  // .refine(
+  //   (data) => {
+  //     return z
+  //       .string()
+  //       .regex(
+  //         new RegExp('^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$')
+  //       )
+  //       .optional()
+  //       .safeParse(data.phone).success;
+  //   },
+  //   {
+  //     message: 'phone field is required for physical goods',
+  //     path: ['phone'],
+  //   }
+  // )
   .refine(
     (data) => {
       if (cartStore.hasPhysicalGoods) {
-        return z.string().min(6).safeParse(data.name).success;
+        return z.string().min(2).safeParse(data.name).success;
       } else return true;
     },
     {
-      message: 'name must be at least 6 characters long refined',
+      message: 'name must be at least 2 characters long',
       path: ['name'],
     }
   )
