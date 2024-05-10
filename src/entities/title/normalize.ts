ab555c64be4560253e232d85903f0298a58157cd
mildfire <mildfire@gmail.com>
2024-05-10 23:10:13 +0500

    good title object normalization

--- src/entities/title/normalize.ts
+++ src/entities/title/normalize.ts
@@ -1,49 +1,58 @@
 import { TitleServer } from './server';
 import { ITitle } from './client';
 
-// export const normalizeAuthor = (data: TitleServer): ITitle => {
-//   return {
-//     ...data,
-//     birthDate: data.birth_date,
-//     deathDate: data.death_date,
-//   };
-// };
+function isEmpty(obj: object) {
+  return Object.keys(obj).length === 0;
+}
 
 export const capFirst = (input: string): string => {
   return input.charAt(0).toUpperCase() + input.slice(1);
 };
 
-export const keyValuePairFuncs = (obj: object | null, outObj: any) => {
-  if (!obj) return; // Added a null check for  Uncaught TypeError: Cannot convert undefined or null to object
+export const keyValuePairFuncs = (
+  obj: object | string | number | null,
+  outObj: any
+) => {
+  if (obj === null) {
+    return null;
+  }
 
-  // const outObj = {} as any;
+  if (typeof obj === 'string') {
+    return obj;
+  }
+
+  if (typeof obj === 'number') {
+    return obj;
+  }
 
   for (const [key, val] of Object.entries(obj)) {
-    console.log(`${key}: ${JSON.stringify(val)}`);
-
     const newKey = `${key}`
       .split('_')
       .map((item, index) => (index === 0 ? item : capFirst(item)))
       .join('');
 
-    if (typeof val !== 'object') outObj[newKey] = val;
+    if (val === null) {
+      outObj[newKey] = null;
+    }
 
-    if (typeof val === 'object') {
+    if (typeof val !== 'object') {
+      outObj[newKey] = val;
+    }
+
+    if (typeof val === 'object' && val !== null) {
       if (Array.isArray(val)) {
-        console.log('found an array !!!');
-
         outObj[newKey] = val.map((item) => {
           const arrOutObj = {} as any;
           return keyValuePairFuncs(item, arrOutObj);
         });
+      } else {
+        outObj[newKey] = {} as any;
+        keyValuePairFuncs(val, outObj[newKey]); // recursively call the function
       }
-
-      outObj[newKey] = {} as any;
-      keyValuePairFuncs(val, outObj[newKey]); // recursively call the function
     }
   }
 
-  return outObj;
+  return isEmpty(outObj) ? null : outObj;
 };
 
 export const normalizeTitle = (data: object) => {
