3928950af99dd7bf5a33ed794a524811c8f39650
mildfire <mildfire@gmail.com>
2024-06-19 20:35:20 +0500

    scroll with bkur effect

--- pages/books/[slug]/index.tsx
+++ pages/books/[slug]/index.tsx
@@ -36,33 +36,12 @@ const BookPage = observer((): React.ReactElement => {
       }
     >
       <Head>
-        {/* <title>Белый цветок</title> */}
-        <meta name='description' content={book.thesis} />
-
-        {/* <meta
-          property='og:url'
-          content='https://mi59173.tw1.ru/books/Belyj-cvetok'
-        /> */}
         <meta property='og:type' content='website' />
-        {/* <meta property='og:title' content='Белыйф цветок' /> */}
-        {/* <meta property='og:description' content='книга белый цветок описание' /> */}
-
-        {/* <meta property='og:image' content={book.cover} /> */}
-
-        {/* <meta
-          property='og:image'
-          content='https://api.mi59173.tw1.ru/storage/v1/object/public/titles/title_title_titles_Belyj-cvetok.jpg'
-        /> */}
-        {/* <meta
-          property='og:image'
-          content='https://ogcdn.net/e4b8c678-7bd5-445d-ba03-bfaad510c686/v4/mi59173.tw1.ru/%D0%91%D0%B5%D0%BB%D1%8B%D0%B9%D1%84%20%D1%86%D0%B2%D0%B5%D1%82%D0%BE%D0%BA/https%3A%2F%2Fopengraph.b-cdn.net%2Fproduction%2Fimages%2F56188dc2-e3c3-4ce5-a8b1-1323953e37b9.jpg%3Ftoken%3DhOY-wLL-tV2Wb6eqlpzb3hUOqYMZbXQ3az2flBDqaSs%26height%3D800%26width%3D1200%26expires%3D33251249770/og.png'
-        /> */}
-
         <meta property='og:title' content={book.name} key='ogtitle' />
         <meta property='og:description' content={book.thesis} key='ogdesc' />
-
         <meta property='og:image' content={book.cover} key='ogimage' />
         <meta property='og:url' content={adress} key='ogurl' />
+
         <meta
           name='description'
           content={
