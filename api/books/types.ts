c7010368058a7f1ade05b73be48294c8a030dbdd
mildfire <mildfire@gmail.com>
2024-12-05 16:57:46 +0500

    usets

--- api/books/types.ts
+++ api/books/types.ts
@@ -6,7 +6,7 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[]
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
       Audiobooks: {
@@ -65,7 +65,7 @@ export interface Database {
             isOneToOne: true
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Authors: {
@@ -130,7 +130,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Authors"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Awards: {
@@ -225,7 +225,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       CardBooks: {
@@ -278,7 +278,7 @@ export interface Database {
             isOneToOne: true
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Cart: {
@@ -340,7 +340,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Courses: {
@@ -438,7 +438,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Magazines"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Ebooks: {
@@ -500,7 +500,7 @@ export interface Database {
             isOneToOne: true
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Extentions: {
@@ -590,7 +590,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Lectors"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Magazine_Issues_Articles_List: {
@@ -616,7 +616,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Magazines"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Magazine_Issues_Recommended_Titles: {
@@ -649,7 +649,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       MagazineArticles: {
@@ -684,7 +684,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Authors"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Magazines: {
@@ -758,7 +758,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       OrderItems: {
@@ -799,7 +799,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Orders"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Orders: {
@@ -876,7 +876,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Printed_Magazine_Issues: {
@@ -935,7 +935,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Magazines"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       PrintedBooks: {
@@ -994,7 +994,7 @@ export interface Database {
             isOneToOne: true
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       PrintedCover: {
@@ -1026,7 +1026,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "PrintedBooks"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       PrintOptions: {
@@ -1061,7 +1061,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "PrintedBooks"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       PrintSize: {
@@ -1090,7 +1090,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "PrintOptions"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Promocodes: {
@@ -1153,7 +1153,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       Subscriptions: {
@@ -1264,7 +1264,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
       TitlesAwards: {
@@ -1297,8 +1297,122 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Titles"
             referencedColumns: ["id"]
-          }
+          },
         ]
+      }
+      users: {
+        Row: {
+          aud: string | null
+          banned_until: string | null
+          confirmation_sent_at: string | null
+          confirmation_token: string | null
+          confirmed_at: string | null
+          created_at: string | null
+          deleted_at: string | null
+          email: string | null
+          email_change: string | null
+          email_change_confirm_status: number | null
+          email_change_sent_at: string | null
+          email_change_token_current: string | null
+          email_change_token_new: string | null
+          email_confirmed_at: string | null
+          encrypted_password: string | null
+          id: string
+          instance_id: string | null
+          invited_at: string | null
+          is_anonymous: boolean
+          is_sso_user: boolean
+          is_super_admin: boolean | null
+          last_sign_in_at: string | null
+          phone: string | null
+          phone_change: string | null
+          phone_change_sent_at: string | null
+          phone_change_token: string | null
+          phone_confirmed_at: string | null
+          raw_app_meta_data: Json | null
+          raw_user_meta_data: Json | null
+          reauthentication_sent_at: string | null
+          reauthentication_token: string | null
+          recovery_sent_at: string | null
+          recovery_token: string | null
+          role: string | null
+          updated_at: string | null
+        }
+        Insert: {
+          aud?: string | null
+          banned_until?: string | null
+          confirmation_sent_at?: string | null
+          confirmation_token?: string | null
+          confirmed_at?: string | null
+          created_at?: string | null
+          deleted_at?: string | null
+          email?: string | null
+          email_change?: string | null
+          email_change_confirm_status?: number | null
+          email_change_sent_at?: string | null
+          email_change_token_current?: string | null
+          email_change_token_new?: string | null
+          email_confirmed_at?: string | null
+          encrypted_password?: string | null
+          id: string
+          instance_id?: string | null
+          invited_at?: string | null
+          is_anonymous?: boolean
+          is_sso_user?: boolean
+          is_super_admin?: boolean | null
+          last_sign_in_at?: string | null
+          phone?: string | null
+          phone_change?: string | null
+          phone_change_sent_at?: string | null
+          phone_change_token?: string | null
+          phone_confirmed_at?: string | null
+          raw_app_meta_data?: Json | null
+          raw_user_meta_data?: Json | null
+          reauthentication_sent_at?: string | null
+          reauthentication_token?: string | null
+          recovery_sent_at?: string | null
+          recovery_token?: string | null
+          role?: string | null
+          updated_at?: string | null
+        }
+        Update: {
+          aud?: string | null
+          banned_until?: string | null
+          confirmation_sent_at?: string | null
+          confirmation_token?: string | null
+          confirmed_at?: string | null
+          created_at?: string | null
+          deleted_at?: string | null
+          email?: string | null
+          email_change?: string | null
+          email_change_confirm_status?: number | null
+          email_change_sent_at?: string | null
+          email_change_token_current?: string | null
+          email_change_token_new?: string | null
+          email_confirmed_at?: string | null
+          encrypted_password?: string | null
+          id?: string
+          instance_id?: string | null
+          invited_at?: string | null
+          is_anonymous?: boolean
+          is_sso_user?: boolean
+          is_super_admin?: boolean | null
+          last_sign_in_at?: string | null
+          phone?: string | null
+          phone_change?: string | null
+          phone_change_sent_at?: string | null
+          phone_change_token?: string | null
+          phone_confirmed_at?: string | null
+          raw_app_meta_data?: Json | null
+          raw_user_meta_data?: Json | null
+          reauthentication_sent_at?: string | null
+          reauthentication_token?: string | null
+          recovery_sent_at?: string | null
+          recovery_token?: string | null
+          role?: string | null
+          updated_at?: string | null
+        }
+        Relationships: []
       }
       Workers: {
         Row: {
@@ -1357,7 +1471,7 @@ export interface Database {
             isOneToOne: false
             referencedRelation: "Workers"
             referencedColumns: ["id"]
-          }
+          },
         ]
       }
     }
@@ -1467,14 +1581,16 @@ export interface Database {
   }
 }
 
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
 export type Tables<
   PublicTableNameOrOptions extends
-    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
@@ -1482,68 +1598,68 @@ export type Tables<
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-      Database["public"]["Views"])
-  ? (Database["public"]["Tables"] &
-      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-      Row: infer R
-    }
-    ? R
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
     : never
-  : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Insert: infer I
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Insert: infer I
-    }
-    ? I
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-    | keyof Database["public"]["Tables"]
+    | keyof PublicSchema["Tables"]
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
       Update: infer U
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-      Update: infer U
-    }
-    ? U
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
-    | keyof Database["public"]["Enums"]
+    | keyof PublicSchema["Enums"]
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    : never = never,
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-  : never
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
 
--- src/components/DashBoardPage/UsersList.tsx
+++ src/components/DashBoardPage/UsersList.tsx
@@ -3,27 +3,29 @@ import { useEffect, useRef, useState } from 'react';
 
 import { Database } from 'api/books/types';
 import { QueryData } from '@supabase/supabase-js';
-import RedLink from '../Common/Link/RedLink';
+// import RedLink from '../Common/Link/RedLink';
 import { Label } from '../ui/label';
 import { Pagination } from './Pagination';
 
-export type OrdersType = Database['public']['Tables']['Orders']['Row'];
-export type OrderItemsType = Database['public']['Tables']['OrderItems']['Row'];
+// export type OrdersType = Database['public']['Tables']['Orders']['Row'];
+// export type OrderItemsType = Database['public']['Tables']['OrderItems']['Row'];
+
+export type UsersType = Database['public']['Tables']['users']['Row'];
 
 type FilterProps = {
-  setFilteredOrders: (orders: OrdersWithItemsType) => void;
-  orders: OrdersWithItemsType;
+  setFilteredUsers: (users: UsersType[]) => void;
+  users: UsersType[];
 };
 
-const Filter = ({ orders, setFilteredOrders }: FilterProps) => {
+const Filter = ({ users, setFilteredUsers }: FilterProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const filterOrders = (orders: OrdersWithItemsType) => {
-    const filteredOrders = orders.filter((order) =>
-      order.email?.includes(inputRef.current?.value || '')
+  const filterUsers = (users: UsersType[]) => {
+    const filteredUsers = users.filter((user) =>
+      user.email?.includes(inputRef.current?.value || '')
     );
 
-    return filteredOrders;
+    return filteredUsers;
   };
 
   return (
@@ -34,47 +36,53 @@ const Filter = ({ orders, setFilteredOrders }: FilterProps) => {
         name='emailInput'
         type='text'
         onChange={() => {
-          const filteredOrders = filterOrders(orders);
-          setFilteredOrders(filteredOrders);
+          const filteredUsers = filterUsers(users);
+          setFilteredUsers(filteredUsers);
         }}
       />
     </div>
   );
 };
 
-const ordersQuery = supabase
-  .from('Orders')
-  .select(`*, items: OrderItems(*)`)
-  .order('created_at', { ascending: false });
+// const ordersQuery = supabase
+//   .from('Orders')
+//   .select(`*, items: OrderItems(*)`)
+//   .order('created_at', { ascending: false });
 
-type OrdersWithItemsType = QueryData<typeof ordersQuery>;
+const usersQuery = supabase
+  .from('users')
+  .select(`*`);
 
-const getOrders = async (): Promise<OrdersWithItemsType> => {
-  const { data } = await ordersQuery;
+type UsersQueryType = QueryData<typeof usersQuery>;
+
+
+
+const getUsers = async (): Promise<UsersQueryType> => {
+  const { data } = await usersQuery;
 
   return data || [];
 };
 
-const OrdersList = ({ perPage }: { perPage: number }) => {
-  const [orders, setOrders] = useState<OrdersWithItemsType>();
-  const [filteredOrders, setFilteredOrders] = useState<OrdersWithItemsType>();
+const UsersList = ({ perPage }: { perPage: number }) => {
+  const [users, setUsers] = useState<UsersQueryType>();
+  const [filteredUsers, setFilteredUsers] = useState<UsersQueryType>();
   const [page, setPage] = useState(1);
 
   const getData = async () => {
-    const orders = await getOrders();
-    orders && setOrders(orders);
-    orders && setFilteredOrders(orders);
+    const users = await getUsers();
+    users && setUsers(users);
+    users && setFilteredUsers(users);
   };
 
   const ordersPerPage = perPage;
-  const totalPages = filteredOrders
-    ? Math.ceil(filteredOrders.length / ordersPerPage)
+  const totalPages = filteredUsers
+    ? Math.ceil(filteredUsers.length / ordersPerPage)
     : 1;
 
   const lastItemIndex = page * ordersPerPage;
   const firstItemIndex = lastItemIndex - ordersPerPage;
-  const currentPageOrders = filteredOrders
-    ? filteredOrders.slice(firstItemIndex, lastItemIndex)
+  const currentPageUsers = filteredUsers
+    ? filteredUsers.slice(firstItemIndex, lastItemIndex)
     : [];
 
   useEffect(() => {
@@ -83,37 +91,42 @@ const OrdersList = ({ perPage }: { perPage: number }) => {
 
   return (
     <div className='flex flex-col gap-6'>
-      {orders && (
-        <Filter orders={orders} setFilteredOrders={setFilteredOrders} />
+      {users && (
+        <Filter users={users} setFilteredUsers={setFilteredUsers} />
       )}
 
-      <div className='grid gap-x-5 gap-y-3 grid-cols-[repeat(6,min-content)]'>
+      <div className='grid gap-x-5 gap-y-3 grid-cols-[repeat(3,max-content)]'>
         <span>id</span>
-        <span>date</span>
-        <span>status</span>
+        {/* <span>date</span>
+        <span>status</span> */}
         <span>email</span>
-        <span>summ</span>
-        <span>info</span>
+        {/* <span>summ</span>
+        <span>info</span> */}
+        <span> is admin </span> 
 
-        {currentPageOrders &&
-          currentPageOrders.map((order) => {
-            const date = new Date(order.created_at);
-            const monthString =
-              date.getMonth() + 1 < 10
-                ? `0${date.getMonth() + 1}`
-                : date.getMonth() + 1;
+        {currentPageUsers &&
+          currentPageUsers.map((user) => {
+            // const date = new Date(order.created_at);
+            // const monthString =
+            //   date.getMonth() + 1 < 10
+            //     ? `0${date.getMonth() + 1}`
+            //     : date.getMonth() + 1;
 
-            const dateString =
-              date.getDate() + '.' + monthString + '.' + date.getFullYear();
+            // const dateString =
+            //   date.getDate() + '.' + monthString + '.' + date.getFullYear();
 
             return (
               <>
-                <span>{order.id}</span>
-                <span>{dateString}</span>
-                <span> {order.status} </span>
-                <span>{order.email}</span>
-                <span>{order.summ}</span>
-                <RedLink href={`/dashboard/orders/` + order.id}> info </RedLink>
+                <span>{user.id}</span>
+                {/* <span>{dateString}</span> */}
+                {/* <span> {order.status} </span> */}
+                <span>{user.email}</span>
+                <span>{
+                   JSON.stringify(user.raw_app_meta_data['claims_admin'], null, 2) 
+                }</span>
+
+                {/* <span>{order.summ}</span> */}
+                {/* <RedLink href={`/dashboard/orders/` + order.id}> info </RedLink> */}
               </>
             );
           })}
@@ -123,4 +136,4 @@ const OrdersList = ({ perPage }: { perPage: number }) => {
   );
 };
 
-export { OrdersList };
+export { UsersList };
